8e80d6c37d5e52fd290ee8d19bb7384346a0da81
medem100 <Modellbauer555@web.de>
2018-10-03 12:08:23 +0200

    vorbereitung auf test api

---
+++ HelloRESTful/src/main/java/org/andre/hellorestful/DataPull.java
@@ -0,0 +1,27 @@
+package org.andre.hellorestful;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/hello")
+public class DataPull {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+}
+ 
